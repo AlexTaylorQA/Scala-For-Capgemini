15fb9c1c13c2ba313709c3e0bd7e70b01ca191c0
AlexTaylorQA <Alexander.Taylor@qa.com>
2017-08-01 21:37:22 +0100

    Added comments.

--- Scala/Application.scala
+++ Scala/Application.scala
@@ -17,13 +17,15 @@ import play.api.libs.json._
 
 class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messagesApi: MessagesApi) extends Controller with I18nSupport with MongoController with ReactiveMongoComponents{
 
-
+// Gets a collection from a Mongo DB ('movies')
   def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("movies"))
 
+// Goes to the Home Page
   def index = Action {
     Ok(views.html.index("Test Output", "Welcome to the home page!"))
   }
 
+// Adds a new movie to the database, then goes to the View Page to display it.
   def toView = Action { implicit request: Request[AnyContent] =>
     val formValidationResult = movies.createMovieForm.bindFromRequest
     formValidationResult.fold({
@@ -36,6 +38,7 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
     })
   }
 
+  // Goes to the View Page (view list of movies)
   def listMovies = Action.async {
 
     val cursor: Future[Cursor[movies]] = collection.map {
@@ -57,12 +60,13 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
   }
 
 
+// Goes to the Edit Page, sending the ID of the movie the user wants to edit.
   def toEdit(theID:Int) = Action {
     val oneMovie = movies.theMovies.zipWithIndex.filter(_._1.mID == theID).head._2
     Ok(views.html.theEdit(movies.theMovies, movies.createMovieForm.fill(movies.theMovies(oneMovie)), oneMovie))
   }
 
-
+ // Edits an existing movie in the Mongo DB, then goes to the View Page to display the edit.
   def doEdit = Action { implicit request: Request[AnyContent] =>
     val formValidationResult = movies.createMovieForm.bindFromRequest
     formValidationResult.fold({
@@ -86,6 +90,7 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
     })
   }
 
+  // Deletes a selected movie from the Mongo DB collection, then goes to the View Page to display the updated movie list.
   def toDel(theID:Int) = Action {
     movies.theMovies.length <= 0 match {
       case true => Ok(views.html.theView(movies.theMovies, movies.createMovieForm))
@@ -95,40 +100,37 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
     }
   }
 
-  def triangle = Action {
-    var str = ""
-    for(x <- 0 to 100)
-      {
-        str += (" &nbsp;" * (100 - x)) + "/" + (" &nbsp;" * x * 2) + "\\<br>"
-
-      }
-    Ok("Test Output\n\n" + str)
-  }
-
+ // Redirects the user to another site.
   def doRedirect = Action {
      Redirect("http://www.google.com")
   }
 
+ // Displays a variable that matches a user input (e.g. '../theDynamic/5'  outputs "The dynamic ID is: 5)
   def theDynamic(id:Int) = Action{
     Ok("The dynamic ID is: " + id)
   }
 
+ // Displays a variable set in the routes file (Output will always be '5')
   def theStatic(id:Int) = Action{
     Ok("The static ID is always: " + id)
   }
 
+  // Displays an ID submitted by the user and states that the default is always '1'.
   def theDefault(id:Int) = Action{
     Ok("The ID given is: " + id + ", and the default ID is always: " + 1 )
   }
 
+  // Displays an ID submitted by the user, or an appropriate message if nothing was submitted.
   def theOptional(id:Option[String]) = Action{
     Ok("The optional ID is: " + id.getOrElse("No ID inputted."))
   }
 
+  // Creates a cookie when the function runs.
   def hello(name: String) = Action{
     Ok("Hello " + name + "!").withCookies(Cookie("theme", "HelloCookie"))
   }
 
+  // Prints the value assigned to the created cookie ('HelloCookie').
   def helloPrintCookie = Action {request =>
      request.cookies.get("theme").map{
       theCookie => Ok("Description: " + theCookie.value)
@@ -144,14 +146,17 @@ class Application @Inject()(val reactiveMongoApi: ReactiveMongoApi, val messages
   }
   */   // Attempted to rename cookie. Cookie name is a val so this failed.
 
+ // Discards a created cookie with a specific identifier ('theme').
   def helloDiscardCookie = Action {
     Ok("The cookie was deleted.").discardingCookies(DiscardingCookie("theme"))
   }
 
+ // Creates a dummy link that leads to a "to be implemented" default page.
   def dummy(name:String) = TODO
 
 
-  // Only one of the DoubleTry functions will be called: doubleTryOne
+  // If two routes named '/doubleTry' lead to separate functions, only one of the 'doubleTry' functions will be called.
+  // (In this case, 'doubleTryOne', the first one).
   def doubleTryOne = Action{
     Ok("The first doubleTry function was called.")
   }
